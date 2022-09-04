18b8ed0f1657af7d9e0e633ad92b2d52af319238
dailianjie <371660929@qq.com>
2022-09-04 11:08:32 +0800

    1.Update 外观设计模式。

--- src/main/java/com/designpattern/art/facade/pattern/Client.java
+++ src/main/java/com/designpattern/art/facade/pattern/Client.java
@@ -1,6 +1,7 @@
 package com.designpattern.art.facade.pattern;
 
 public class Client {
+
     public static void main(String[] args) {
         CopyFacade copyFacade = new CopyFacade();
         copyFacade.oneKeyCopy();
